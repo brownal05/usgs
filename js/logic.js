ea0b9029387000aa6f3692bb0308a1fa33d72812
Alexander Brown <alex.brown.f@gmail.com>
2019-03-31 15:45:55 -0500

    working on the legend

--- js/logic.js
+++ js/logic.js
@@ -11,12 +11,13 @@ var baseMaps = {
   };
 
   var overlayMaps = {
-    "Major Earthquakes": majorQuakes
+    "Major Earthquakes": majorQuakes,
+    // "Plate Boundries" : majorPlates
   };
 
   var map = L.map("map-id", {
     center: [37.68, -97.33],
-    zoom: 10,
+    zoom: 5,
     layers: [lightmap, majorQuakes]
   });  
 //   var map = L.map("map-id", {
@@ -38,16 +39,23 @@ lightmap.addTo(map);
 // }
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
-var info = L.control({
+var legend = L.control({
     position: "topright"
   });
 
-  info.onAdd = function() {
+  legend.onAdd = function() {
     var div = L.DomUtil.create("div", "legend");
+    var lables = "<h1>0-1</h1>" +
+    "<div class=\"labels\">" +
+      "<div class=\"min\">" + limits[0] + "</div>" +
+      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    "</div>";
     return div;
   };
 
-  info.addTo(map);
+  legend.addTo(map);
+
+  
 }
 
 function createMarkers(response) {
@@ -56,14 +64,64 @@ function createMarkers(response) {
   
     for (var i=0; i < quakes.length; i++) {
         var quake = quakes[i];
-        console.log(quake)
-        var quakeMarker = L.marker([quake.geometry.coordinates[1], quake.geometry.coordinates[0]])
-            .bindPopup("<h3>" + quake.properties.mag + "<h3><h3>Time: " + quake.properties.place + "<h3>");
+        var quakeMarker = L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+            color : chooseColor(quake.properties.mag),
+            fillColor : chooseColor(quake.properties.mag),
+            fillOpacity : 1,
+            radius:  quake.properties.mag * 15000
+        })
+            .bindPopup("<h3>Magnitude: " + quake.properties.mag + "<h3><h3>Time: " + quake.properties.place + "<h3>");
         quakeMarkers.push(quakeMarker);
     }
     let markerLayerGroup = L.layerGroup(quakeMarkers);
     createQuakes(markerLayerGroup);
 }
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 d3.json(url, createMarkers);
+function chooseColor(mag) {
+    switch (true) {
+    case  mag <= 1:
+      return "#fefcea"
+      break;      
+    case mag <= 2 :
+      return "#c5e534 ";
+      break;
+    case mag <= 3:
+      return "#c5e534";
+      break;
+    case mag <= 4 :
+      return "#f1f438";
+      break;
+    case mag <= 5 :
+      return "#f1f438";
+      break
+    case mag <= 6 :
+      return "#e29f34";
+      break
+    case mag <= 7 :
+      return "#f93939";
+    }
+  }
 
+
+
+
+
+// function createLines(response) {
+//     var plates= response;
+//     var plateBoundries = []
+  
+//     for (var i=0; i < plates.length; i++) {
+//         var plate = plates[i];
+
+//         var plateBoundry = L.polyline([plate.coordinates[1], quake.coordinates[0]])
+//         console.log(plate)
+
+//         plateBoundries.push(plateBoundry);
+//     }
+//     let lineLayerGroup = L.layerGroup(plateBoundries);
+//     createQuakes(lineLayerGroup);
+
+// }
+// let lineUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+// d3.json(lineUrl, createLines)
